4c28c9217b53c5c2c774aa77dc7ad5f5279c4530
Damian Mroz <damianmroz141@gmail.com>
2024-04-25 11:52:02 +0200

    Change task_1.py

--- task_1.py
+++ task_1.py
@@ -1,17 +1,7 @@
-list_1=[]
-list_2=[]
-def palindrome(a):
-    for element in a:
-        list_1.append(element)
-    for element2 in reversed(a):
-        list_2.append(element2)
-    
-def fun():
-    if list_1==list_2:
+def palindrome_check(word):
+    if word == word[::-1]:
         return True
     else:
         return False
-    
-palindrome("enter word here")
 
-print(fun())
+print(palindrome_check("enter word here"))
