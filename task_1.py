7e0226c18c2adcdc346d2a9fbb2e52a7c7168435
Damian Mroz <damianmroz141@gmail.com>
2024-04-25 15:31:57 +0200

    Modify task_1.py

--- task_1.py
+++ task_1.py
@@ -1,7 +1,10 @@
-def palindrome_check(word):
-    if word == word[::-1]:
-        return True
-    else:
-        return False
+import string
+from string import punctuation
 
-print(palindrome_check("enter word here"))
+def palindrome_check(word):
+    trans = str.maketrans('', '', string.punctuation)
+    new_word=word.translate(trans).translate \
+   ({ord(c): None for c in  string.whitespace})
+    return new_word[::-1].casefold() == new_word.casefold()
+    
+print(palindrome_check("Kajak"))
